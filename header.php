490bfc297b8923f69e9b613c4696c96844f9e862
clementtirilly <clement.tirilly@arece.eu>
2025-04-24 15:36:38 +0200

    Add files via upload
    Voilà la v1 de la navbar, dites-moi si vous avez d'autres suggestions

---
+++ header.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-sm navbar-light bg-light border-bottom">
+    <div class="container">
+        <a href="index.php" class="navbar-brand mb-0 h1"> 
+            <img src="img/logosite.png"  class="logonav" alt="Logo du site">
+            <img src="img/slogan.png"  class="slognav" alt="Slogan du site">
+        </a>
+        
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarECE" aria-controls="navbarECE" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="navbarECE">
+            <div class="d-flex gap-2">
+            <a href="index.php" class="btn btn-outline-primary">Accueil</a>
+            <a href="add_vdece.php" class="btn btn-primary">+ Poster</a>
+            </div>
+        </div>
+    </div>
+</nav>
